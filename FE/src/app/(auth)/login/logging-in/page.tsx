65bccc3b4b7fb6835b5551090bc7a50e4985881b
bada308 <203190@jnu.ac.kr>
2024-03-24 01:13:56 +0900

    fix: 로그인 요청 제거

--- FE/src/app/(auth)/login/logging-in/page.tsx
+++ FE/src/app/(auth)/login/logging-in/page.tsx
@@ -3,20 +3,21 @@
 import { useSearchParams } from "next/navigation";
 import { useEffect } from "react";
 import LoadingSpinner from "@/components/common/LoadingSpinner";
-import { useSlackLoginMutation } from "@/hooks/query/useAuthQuery";
+// import { useSlackLoginMutation } from "@/hooks/query/useAuthQuery";
 
 const LoginSlackCallbackPage = () => {
   const searchParams = useSearchParams();
   const code = searchParams.get("code");
 
-  const { mutate: loginSlack } = useSlackLoginMutation();
+  // const { mutate: loginSlack } = useSlackLoginMutation();
 
   useEffect(() => {
     if (code) {
-      loginSlack({
-        code,
-        redirect_uri: process.env.NEXT_PUBLIC_SLACK_REDIRECT_URI,
-      });
+      // loginSlack({
+      //   code,
+      //   redirect_uri: process.env.NEXT_PUBLIC_SLACK_REDIRECT_URI,
+      // });
+      console.log("code", code);
     }
   }, [code]);
 
