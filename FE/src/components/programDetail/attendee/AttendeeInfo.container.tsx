182b0a70d18f0e1ad9160bc6ae3f28d4a4271246
박건규 <geongyu09@gmail.com>
2024-04-05 16:02:35 +0900

    feat: 블러처리 목 데이터 추가
    블러처리 목 데이터 추가
    
    #9

--- FE/src/components/programDetail/attendee/AttendeeInfo.container.tsx
+++ FE/src/components/programDetail/attendee/AttendeeInfo.container.tsx
@@ -22,13 +22,17 @@ const AttendeeInfoContainer = ({ programId }: AttendeeInfoContainerProps) => {
   return (
     <ErrorBoundary FallbackComponent={ErrorFallback}>
       <div className="space-y-16">
-        {isLoggedIn ? (
-          attendStatuses.map((status) => (
-            <AttendeeInfo key={status} programId={programId} status={status} />
-          ))
-        ) : (
-          <BluredAttedee />
-        )}
+        {isLoggedIn
+          ? attendStatuses.map((status) => (
+              <AttendeeInfo
+                key={status}
+                programId={programId}
+                status={status}
+              />
+            ))
+          : attendStatuses.map((status) => (
+              <BluredAttedee key={status} status={status} />
+            ))}
       </div>
     </ErrorBoundary>
   );
--- FE/src/components/programDetail/attendee/BluredAttedee.tsx
+++ FE/src/components/programDetail/attendee/BluredAttedee.tsx
@@ -1,25 +1,35 @@
-import { attendStatuses } from "./AttendeeInfo.container";
 import AttendeeStatus from "./AttendeeStatus";
 import { MemberAttendStatusInfoDto } from "@/apis/dtos/member.dto";
 import MemberList from "@/components/common/MemberList";
+import { AttendStatus } from "@/types/member";
 
-const members: MemberAttendStatusInfoDto[] = [
-  {
-    memberId: 1,
-    name: "김민수",
-    attendStatus: "attend",
-  },
-];
+const mockMember: MemberAttendStatusInfoDto = {
+  memberId: 1,
+  name: "20기 박건규",
+  attendStatus: "attend",
+};
 
-export default function BluredAttedee() {
+interface BluredAttedeeProps {
+  status: AttendStatus;
+}
+
+export default function BluredAttedee({ status }: BluredAttedeeProps) {
+  const population =
+    status === "attend"
+      ? 52
+      : status === "late"
+      ? 3
+      : status === "absent"
+      ? 1
+      : 0;
+  const members = Array.from({ length: population }, (_, i) => ({
+    ...mockMember,
+    memberId: i + 1,
+  }));
   return (
     <div className="space-y-16">
-      {attendStatuses.map((status) => (
-        <div>
-          <AttendeeStatus status={status} members={members} />
-          <MemberList members={members} blur />
-        </div>
-      ))}
+      <AttendeeStatus status={status} members={members} />
+      <MemberList members={members} blur />
     </div>
   );
 }
