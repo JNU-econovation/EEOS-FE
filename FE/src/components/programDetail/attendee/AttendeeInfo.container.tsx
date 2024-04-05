17e031c7fc4f57b371cb3e3c5233ab00b5e2de07
박건규 <geongyu09@gmail.com>
2024-04-05 15:50:36 +0900

    refactor: 반복되는 컴포넌트 호출을 반복문으로 치환
    반복되는 컴포넌트 호출을 반복문으로 변경
    
    #9

--- FE/src/components/programDetail/attendee/AttendeeInfo.container.tsx
+++ FE/src/components/programDetail/attendee/AttendeeInfo.container.tsx
@@ -2,6 +2,7 @@
 
 import { ErrorBoundary } from "react-error-boundary";
 import AttendeeInfo from "./AttendeeInfo";
+import BluredAttedee from "./BluredAttedee";
 import ErrorFallback from "@/components/common/ErrorFallback";
 import { CheckIsLoggedIn } from "@/utils/authWithStorage";
 
@@ -9,20 +10,25 @@ interface AttendeeInfoContainerProps {
   programId: number;
 }
 
+export const attendStatuses = [
+  "attend",
+  "late",
+  "absent",
+  "nonResponse",
+] as const;
+
 const AttendeeInfoContainer = ({ programId }: AttendeeInfoContainerProps) => {
   const isLoggedIn = CheckIsLoggedIn();
   return (
     <ErrorBoundary FallbackComponent={ErrorFallback}>
       <div className="space-y-16">
-        {!isLoggedIn && (
-          <div className="text-center text-red-500">
-            로그인이 필요한 페이지입니다.
-          </div>
+        {isLoggedIn ? (
+          attendStatuses.map((status) => (
+            <AttendeeInfo key={status} programId={programId} status={status} />
+          ))
+        ) : (
+          <BluredAttedee />
         )}
-        <AttendeeInfo programId={programId} status="attend" />
-        <AttendeeInfo programId={programId} status="late" />
-        <AttendeeInfo programId={programId} status="absent" />
-        <AttendeeInfo programId={programId} status="nonResponse" />
       </div>
     </ErrorBoundary>
   );
