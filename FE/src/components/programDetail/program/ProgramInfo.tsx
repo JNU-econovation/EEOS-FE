894a6f4c8b48ed5e3876d836b105ca07e2febe48
박건규 <geongyu09@gmail.com>
2024-04-11 23:46:05 +0900

    chore: 주석 추가
    임시 기능으로 추가한 버튼, 기능 구현 완료시 제거하도록 TODO 주석 추가
    
    #15

--- FE/src/components/programDetail/program/ProgramInfo.tsx
+++ FE/src/components/programDetail/program/ProgramInfo.tsx
@@ -3,7 +3,8 @@
 import ProgramDetail from "./ProgramDetail";
 import ProgramHeader from "./ProgramHeader";
 import ProgramInfoLoader from "./ProgramInfo.loader";
-import { sendMessage } from "@/apis/program";
+// TODO: 기능 구현 이후 삭제하기
+import { sendSlackMessage } from "@/apis/program";
 import { useGetProgramById } from "@/hooks/query/useProgramQuery";
 
 interface ProgramInfoProps {
@@ -22,7 +23,10 @@ const ProgramInfo = ({ programId }: ProgramInfoProps) => {
 
   return (
     <section className="space-y-8">
-      <button onClick={() => sendMessage(+programId)}>메시지 보내기</button>
+      {/* TODO: 기능 구현 이후 삭제하기 */}
+      <button onClick={() => sendSlackMessage(+programId)}>
+        메시지 보내기
+      </button>
       <ProgramHeader data={programData} />
       <ProgramDetail data={programData} />
     </section>
