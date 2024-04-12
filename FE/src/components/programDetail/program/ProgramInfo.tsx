cfeadadb1ad64bace42b8c9bf4892ba5ed361f5c
박건규 <geongyu09@gmail.com>
2024-04-12 14:09:38 +0900

    feat: 테스트용 버튼 제거
    테스트용 버튼 제거
    
    #15

--- FE/src/components/programDetail/program/ProgramInfo.tsx
+++ FE/src/components/programDetail/program/ProgramInfo.tsx
@@ -3,8 +3,6 @@
 import ProgramDetail from "./ProgramDetail";
 import ProgramHeader from "./ProgramHeader";
 import ProgramInfoLoader from "./ProgramInfo.loader";
-// TODO: 기능 구현 이후 삭제하기
-import { sendSlackMessage } from "@/apis/program";
 import { useGetProgramById } from "@/hooks/query/useProgramQuery";
 
 interface ProgramInfoProps {
@@ -23,10 +21,6 @@ const ProgramInfo = ({ programId }: ProgramInfoProps) => {
 
   return (
     <section className="space-y-8">
-      {/* TODO: 기능 구현 이후 삭제하기 */}
-      <button onClick={() => sendSlackMessage(+programId)}>
-        메시지 보내기
-      </button>
       <ProgramHeader data={programData} />
       <ProgramDetail data={programData} />
     </section>
