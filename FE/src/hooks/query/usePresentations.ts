69d3f207d0f2496542ea881b4d0e78f9d4180375
박건규 <geongyu09@gmail.com>
2024-07-11 00:13:01 +0900

    feat($행사|팀관리): 발표 자료 요청 query 추가(usePresentation.ts)
    발표 자료 요청 쿼리문 추가
    
    요청을 위한 프로그램별 깃허브 url은 행사 detail 페이지에서 요청하는 곳에서 가져오며(useProgramQuery.ts), 여기에서 url을 캐싱을 해준다.
    
    이후 발표자료를 요청할 때, 해당 캐싱된 값을 가져와서 요청하도록 함
    
    #43

--- FE/src/hooks/query/usePresentations.ts
+++ FE/src/hooks/query/usePresentations.ts
@@ -1,10 +1,17 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { getPresentations } from "@/apis/proxy/github";
 
-const usePresentations = (githubUrl: string) => {
+const usePresentations = (programId: number) => {
+  const queryClient = useQueryClient();
   return useQuery({
-    queryKey: ["presentations", githubUrl],
-    queryFn: () => getPresentations(githubUrl),
+    queryKey: ["presentations", programId],
+    queryFn: () => {
+      const githubUrl = queryClient.getQueryData(["githubUrl", programId]);
+      if (!githubUrl) return;
+      return getPresentations(githubUrl as string);
+    },
+    staleTime: 1000 * 60 * 60 * 24,
+    cacheTime: 1000 * 60 * 60 * 24,
   });
 };
 
