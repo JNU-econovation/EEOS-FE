7c929ebfe8550c4e027350f1bfd5b5aa98def58c
박건규 <geongyu09@gmail.com>
2024-07-11 14:21:19 +0900

    fix($행사): 발표자료가 새 탭에서 열리도록 수정
    발표자료는 깃허브 링크로, 기존 eeos서비스와는 다른 페이지로 이동하기에
    eeos 이탈율 감소 및 사용자가 여러 자료에 들어갔다 나와았다 하기에, 이에 편리성을 위하여
    발표자료를 새 탭에서 열리도록 수정

--- FE/src/components/programDetail/program/ProgramPresentations.tsx
+++ FE/src/components/programDetail/program/ProgramPresentations.tsx
@@ -14,11 +14,17 @@ const ProgramPresentations = ({ programId }: ProgramPresentationsProps) => {
   return (
     <section>
       <Title text="발표자료 " />
+      {/* TODO: 로더 적용하기 */}
       {isLoading && <div>로딩중...</div>}
       <div className="mx-auto mt-8 grid w-fit gap-x-40 gap-y-8 px-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
         {presentations &&
           presentations.map(({ download_url, name }) => (
-            <Link key={name} className="flex gap-4" href={download_url}>
+            <Link
+              key={name}
+              className="flex gap-4"
+              href={download_url}
+              target="_blank"
+            >
               <Image
                 src="/icons/folder.svg"
                 width={20}
