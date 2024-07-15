b2840dbe6e02792c099866ba48bb422dddd2029b
박건규 <geongyu09@gmail.com>
2024-07-15 15:53:09 +0900

    fix: 깃허브 url이 존재할 때만 요청을 보내도록 수정 (usePresentations.ts)
    #56

--- FE/src/hooks/query/usePresentations.ts
+++ FE/src/hooks/query/usePresentations.ts
@@ -3,15 +3,15 @@ import { getPresentations } from "@/apis/proxy/github";
 
 const usePresentations = (programId: number) => {
   const queryClient = useQueryClient();
+  const githubUrl = queryClient.getQueryData(["githubUrl", programId]);
   return useQuery({
     queryKey: ["presentations", programId],
     queryFn: () => {
-      const githubUrl = queryClient.getQueryData(["githubUrl", programId]);
-      if (!githubUrl) return;
       return getPresentations(githubUrl as string);
     },
     staleTime: 1000 * 60 * 60 * 24,
     cacheTime: 1000 * 60 * 60 * 24,
+    enabled: !!githubUrl,
   });
 };
 
