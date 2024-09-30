218a971706f7bbc504a16313786f93edce3fd202
박건규 <geongyu09@gmail.com>
2024-09-30 23:31:15 +0900

    feat: 해당 프로그램의 출석 상태를 확인하여 출석중일시에 수정 페이지로 접근할 수 있는 버튼 숨기기
    #117

--- FE/src/components/programDetail/program/ProgramHeader.tsx
+++ FE/src/components/programDetail/program/ProgramHeader.tsx
@@ -3,6 +3,7 @@ import { ProgramInfoDto } from "@/apis/dtos/program.dto";
 import TabItem from "@/components/common/tabs/tab/TabItem";
 import Title from "@/components/common/Title/Title";
 import PROGRAM from "@/constants/PROGRAM";
+import { useGetProgramByProgramId } from "@/hooks/query/useProgramQuery";
 import { formatTimestamp } from "@/utils/convert";
 
 interface ProgramHeaderProps {
@@ -13,6 +14,11 @@ const DEADLINE_TEXT = "행사일정 : ";
 
 const ProgramHeader = ({ data }: ProgramHeaderProps) => {
   const { category, title, deadLine, programId, accessRight } = data;
+  const isAbleToEdit = accessRight === "edit";
+  const { data: programData, status } = useGetProgramByProgramId(
+    programId,
+    isAbleToEdit,
+  );
 
   const categoryText = PROGRAM.CATEGORY_TAB[category]?.text ?? "기타";
 
@@ -24,9 +30,11 @@ const ProgramHeader = ({ data }: ProgramHeaderProps) => {
         <p className="sm:text-lg">
           {DEADLINE_TEXT + formatTimestamp(deadLine)}
         </p>
-        {accessRight === "edit" && (
-          <EditAndDeleteButton programId={programId} />
-        )}
+        {(accessRight === "edit" &&
+          programData?.attendMode === "end") ||
+          (programData?.attendMode === "non_open" && (
+            <EditAndDeleteButton programId={programId} />
+          ))}
       </div>
     </section>
   );
