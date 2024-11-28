93f78059cb6b20ab7e5cc8114624ff7cfc935235
박건규 <geongyu09@gmail.com>
2024-11-28 14:22:53 +0900

    refactor: CheckBox 컴포넌트 조금 더 재사용 가능하게 상위에서 classname을 받을 수 있도록 수정
    #141

--- FE/src/components/common/CheckBox/CheckBox.tsx
+++ FE/src/components/common/CheckBox/CheckBox.tsx
@@ -5,15 +5,22 @@ interface CheckBoxProps {
   checked: boolean;
   onClick: () => void;
   disabled?: boolean;
+  className: string;
 }
 
-const CheckBox = ({ checked, onClick, disabled = false }: CheckBoxProps) => {
+const CheckBox = ({
+  checked,
+  onClick,
+  disabled = false,
+  className,
+}: CheckBoxProps) => {
   const checkboxClass = classNames(
     "flex h-6 w-6 items-center justify-center rounded border-2 transition duration-100",
     checked ? "border-blue-500 bg-blue-500" : "border-gray-20 bg-background",
     {
       "cursor-not-allowed opacity-0": disabled,
     },
+    className,
   );
 
   const handleCheckBoxClick = () => {
