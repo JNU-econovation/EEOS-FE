c44fbb4f3e14b0dafab3a9a4a596a436bcca0e25
박건규 <geongyu09@gmail.com>
2024-11-28 14:21:32 +0900

    feat: 익명 데이터 추가로 변경된 api 수정
    #141

--- FE/src/apis/question.ts
+++ FE/src/apis/question.ts
@@ -11,22 +11,34 @@ export const getQuestionsByTeam = async (programId: number, teamId: number) => {
   return new QuestionListDto(data?.data);
 };
 
+/**
+ * 질문을 등록합니다.
+ * - isCheck : 질문을 등록할 때 체크박스를 체크했는지 여부. 체크가 되었다면 1, 아니라면 0
+ */
 export interface PostQuestionParams {
   programId: number;
   teamId: number;
   questionContent: string;
   parentsCommentId?: number;
+  isChecked: 0 | 1;
 }
 export const postQuestion = async ({
   programId,
   teamId,
   questionContent,
   parentsCommentId = -1,
+  isChecked,
 }: PostQuestionParams) => {
   return await https({
     url: API.QUESTION.CREATE,
     method: "POST",
-    data: { programId, teamId, content: questionContent, parentsCommentId },
+    data: {
+      programId,
+      teamId,
+      content: questionContent,
+      parentsCommentId,
+      isChecked,
+    },
   });
 };
 
