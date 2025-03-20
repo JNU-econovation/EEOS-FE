09131c763558be872291bfda326dffea0ed2eec9
bada308 <gangbada890@gmail.com>
2025-03-21 00:26:57 +0900

    feat: User 버튼 클릭 시 모달 제거 및 마이페이지 이동 로직 추가

--- FE/src/components/common/header/UserBtn.tsx
+++ FE/src/components/common/header/UserBtn.tsx
@@ -1,21 +1,13 @@
 import Image from "next/image";
-import UserActiveModal from "./Modal/UserActiveModal";
-import useModal from "@/hooks/useModal";
-import useOutsideRef from "@/hooks/useOutsideRef";
+import { useRouter } from "next/navigation";
 
 const UserBtn = () => {
-  const { isOpen, openModal, closeModal } = useModal();
-  const modalRef = useOutsideRef(closeModal);
-
-  const handleClick = () => {
-    isOpen ? closeModal() : openModal();
-  };
+  const router = useRouter();
 
   return (
     <button
-      ref={modalRef}
       className="relative cursor-pointer"
-      onClick={handleClick}
+      onClick={() => router.push("/mypage")}
     >
       <Image
         src="/icons/user.svg"
@@ -24,7 +16,6 @@ const UserBtn = () => {
         height={28}
         className="h-[28px] w-[28px]"
       />
-      {isOpen && <UserActiveModal />}
     </button>
   );
 };
