331d003261368f72b3bdc3d8ed29932a6ea9633b
bada308 <gangbada890@gmail.com>
2025-03-20 22:51:33 +0900

    feat: 본인의 출석 현황 관련 API 및 DTO 추가 - ATTENDANCE_LIST, ATTENDANCE_SUMMARY API 경로 정의 - PageInfoDto, AttendanceInfoDto, UserAttendanceListDto, UserAttendanceSummaryDto 추가

--- FE/src/apis/dtos/member.dto.ts
+++ FE/src/apis/dtos/member.dto.ts
@@ -5,7 +5,10 @@ import type {
   MemberActiveStatusInfo,
   MemberAttendStatusInfo,
   MemberInfo,
+  PageInfo,
+  UserAttendanceSummary,
 } from "@/types/member";
+import { ProgramStatus } from "@/types/program";
 
 export class MemberDto {
   public readonly memberId: number;
@@ -43,5 +46,64 @@ export class MemberActiveStatusInfoDto extends MemberDto {
   constructor(data: MemberActiveStatusInfo) {
     super(data);
     this.activeStatus = data?.activeStatus;
+  }
+}
+
+class PageInfoDto {
+  public readonly total: number;
+  public readonly current: number;
+  public readonly size: number;
+
+  constructor(data: PageInfo) {
+    this.total = data?.total;
+    this.current = data?.current;
+    this.size = data?.size;
+  }
+}
+
+interface AttendanceInfo {
+  programId: number;
+  title: string;
+  programStatus: ProgramStatus;
+  attendStatus: AttendStatus;
+}
+
+export class AttendanceInfoDto {
+  public readonly programId: number;
+  public readonly title: string;
+  public readonly programStatus: ProgramStatus;
+  public readonly attendStatus: AttendStatus;
+
+  constructor(data: AttendanceInfo) {
+    this.programId = data.programId;
+    this.title = data.title;
+    this.programStatus = data.programStatus;
+    this.attendStatus = data.attendStatus;
+  }
+}
+
+export class UserAttendanceListDto {
+  public readonly pageInfo: PageInfoDto;
+  public readonly attendances: AttendanceInfo[];
+
+  constructor(data: any) {
+    this.pageInfo = new PageInfoDto(data?.pageInfo);
+    this.attendances = data?.page.map(
+      (item: AttendanceInfo) => new AttendanceInfoDto(item),
+    );
+  }
+}
+
+export class UserAttendanceSummaryDto {
+  public readonly attendCount: number;
+  public readonly lateCount: number;
+  public readonly absentCount: number;
+  public readonly paneltyPoint: number;
+
+  constructor(data: UserAttendanceSummary) {
+    this.attendCount = data?.attendCount;
+    this.lateCount = data?.lateCount;
+    this.absentCount = data?.absentCount;
+    this.paneltyPoint = data?.paneltyPoint;
   }
 }
