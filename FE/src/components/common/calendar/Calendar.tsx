030e7484f58591101e42d24fff7d71e08948c289
박건규 <geongyu09@gmail.com>
2025-06-28 14:48:00 +0900

    feat: Calendar 컴포넌트에서 시간까지 입력받을 수 있는 input 추가
    Calendar컴포넌트에 시간을 입력받을 수 있는 input을 추가하였습니다.
    이는 withTime props를 주었을 때에만 사용이 가능합니다.
    
    #190

--- FE/src/components/common/calendar/Calendar.tsx
+++ FE/src/components/common/calendar/Calendar.tsx
@@ -1,23 +1,51 @@
 import { DayPicker } from "react-day-picker";
 import "react-day-picker/dist/style.css";
 import "./calendar.styles.css";
+import { useState } from "react";
 
 interface CalendarProps {
   date: Date | undefined;
+  withTime?: boolean;
   handleDateChange: (date: Date | undefined) => void;
 }
 
-const Calendar = ({ date, handleDateChange }: CalendarProps) => {
+const Calendar = ({ date, withTime, handleDateChange }: CalendarProps) => {
   const disabledDays = { before: new Date() };
 
+  const [timeValue, setTimeValue] = useState<string>("00:00");
+
   return (
-    <DayPicker
-      mode="single"
-      selected={date}
-      onSelect={handleDateChange}
-      disabled={disabledDays}
-      className="absolute left-0 top-[4.5rem] z-10 rounded-md bg-background p-3 shadow-md"
-    />
+    <div className="absolute left-0 top-[4.5rem] z-10 rounded-md bg-background p-3 shadow-md">
+      <DayPicker
+        mode="single"
+        selected={date}
+        onSelect={(e) => {
+          handleDateChange(e);
+        }}
+        disabled={disabledDays}
+      />
+      {withTime && (
+        <>
+          <form style={{ marginBlockEnd: "1em" }}>
+            <label>
+              Set the time:{" "}
+              <input
+                type="time"
+                value={timeValue}
+                onChange={(e) => {
+                  const [hours, minutes] = e.target.value.split(":");
+                  const newDate = date ? new Date(date) : new Date();
+                  newDate.setHours(parseInt(hours, 10));
+                  newDate.setMinutes(parseInt(minutes, 10));
+                  handleDateChange(newDate);
+                  setTimeValue(e.target.value);
+                }}
+              />
+            </label>
+          </form>
+        </>
+      )}
+    </div>
   );
 };
 export default Calendar;
