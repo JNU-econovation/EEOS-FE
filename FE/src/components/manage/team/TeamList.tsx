fbb2ca006f45ecb7b4286d415ceae65688476ae5
bada308 <gangbada890@gmail.com>
2025-06-29 20:49:23 +0900

    feat: 팀 선택 UI 변경

--- FE/src/components/manage/team/TeamList.tsx
+++ FE/src/components/manage/team/TeamList.tsx
@@ -1,6 +1,7 @@
 import classNames from "classnames";
 import Image from "next/image";
 import { useDeleteTeam } from "@/hooks/query/useTeamQuery";
+import Check from "@/components/icons/items/Check";
 
 interface TeamListProps {
   type?: "select" | "manage";
@@ -22,42 +23,31 @@ const TeamList = ({
   };
 
   const listClassName = classNames(
-    "flex select-none justify-between rounded border-2 px-4 py-2 shadow-sm",
+    "flex select-none justify-between rounded border px-4 py-2 shadow-sm transition-colors duration-300",
     {
-      "bg-success-10": isSelected,
+      "border-success-30 bg-success-10 hover:bg-success-30/10": isSelected,
+      "hover:bg-gray-10": !isSelected,
     },
   );
   return (
-    <li className={listClassName}>
+    <li className={classNames(listClassName, "flex items-center ")}>
       <span>{teamName}</span>
-      {type == "manage" && (
+      {type === "manage" && (
         <div onClick={() => deleteSelectedTeam(teamId)}>
           <Image
             src="/icons/trash.svg"
-            width={22}
-            height={22}
+            width={18}
+            height={18}
             alt="Delete Btn"
           />
         </div>
       )}
-      {isSelected && type == "select" && (
-        <Image
-          src="/icons/non_select.svg"
-          width={22}
-          height={22}
-          alt="Delete Btn"
-        />
-      )}
-      {!isSelected && type == "select" && (
-        <Image
-          src="/icons/select.svg"
-          width={22}
-          height={22}
-          alt="Delete Btn"
-        />
-      )}
+      {type === "select" && <div className={classNames("w-5 flex items-center justify-center h-5 border-2 rounded-full", isSelected ? "border-success-30 bg-success-30" : "border-gray-300 bg-none")}>{
+          isSelected && <Check className="fill-white w-3 h-3 stroke-white" />
+      }</div>}
     </li>
   );
 };
 
 export default TeamList;
+
