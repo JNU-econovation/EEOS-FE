72ed44220541121f157b4871cdbe6df62513f7c5
박건규 <geongyu09@gmail.com>
2025-08-24 14:06:57 +0900

    feat: FloatingButton 클릭시 캘린더가 보이도록 기능 구현

--- FE/src/components/feature/floating/FloatingButton.tsx
+++ FE/src/components/feature/floating/FloatingButton.tsx
@@ -1,17 +1,30 @@
-import Image from "next/image";
+"use client";
 
+import Image from "next/image";
+import { useState } from "react";
+import EventCalender from "./EventCalender";
+
+// TODO: 현재 캘린더릴 띄우는 동작과 ui 가 섞여있음. 리팩토링 필요
 const FloatingButton = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <div className="absolute bottom-14 right-14">
-      <button className="rounded-full bg-slate-50 p-4 text-white shadow-lg">
-        <Image
-          src="/icons/eeos_logo.svg"
-          alt="Floating Button"
-          width={24}
-          height={24}
-        />
-      </button>
-    </div>
+    <>
+      <div className="fixed bottom-14 right-14 z-30">
+        <button
+          className="rounded-full bg-slate-50 p-4 text-white shadow-lg"
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          <Image
+            src="/icons/eeos_logo.svg"
+            alt="Floating Button"
+            width={24}
+            height={24}
+          />
+        </button>
+      </div>
+      {isOpen && <EventCalender onClose={() => setIsOpen(false)} />}
+    </>
   );
 };
 
