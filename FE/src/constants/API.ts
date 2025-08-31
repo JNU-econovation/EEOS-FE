0347f2f692ae13e65462f6a0ff875377751d1ab5
박건규 <geongyu09@gmail.com>
2025-08-31 21:41:58 +0900

    feat: 부서 업데이트 api 주소 상수 데이터 추가

--- FE/src/constants/API.ts
+++ FE/src/constants/API.ts
@@ -1,3 +1,5 @@
+import type { Department } from "@/types/member";
+
 const PROGRAM = {
   LIST: "/programs",
   CREATE: "/programs",
@@ -22,6 +24,8 @@ const MEMBER = {
     `/attend/programs/fire-finger/${programId}`,
   ATTENDANCE_LIST: "/attend/programs",
   ATTENDANCE_SUMMARY: "/attend/summary",
+  UPDATE_DEPARTMENT: (userId: number, department: Department) =>
+    `/members/${userId}/department?to=${department}`,
 };
 
 const USER = {
