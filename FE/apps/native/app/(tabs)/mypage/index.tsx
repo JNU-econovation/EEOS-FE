13c218546041963d132c62b1585b67420f083ad1
bada308 <gangbada890@gmail.com>
2025-11-15 23:16:18 +0900

    feat: 페이지별 layout, index 추가

--- FE/apps/native/app/(tabs)/mypage/index.tsx
+++ FE/apps/native/app/(tabs)/mypage/index.tsx
@@ -3,7 +3,7 @@ import { Text, View } from "react-native";
 export default function MypageScreen() {
   return (
     <View>
-      <Text>Mypage</Text>
+      <Text className="text-blue-500">Mypage</Text>
     </View>
   );
 }
