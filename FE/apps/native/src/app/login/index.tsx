c411a9dd2127958030b741dd3ffae1bc36f9d9f3
박건규 <geongyu09@gmail.com>
2026-01-02 22:44:01 +0900

    feat: 앱 로그인 페이지 ui 구현

--- FE/apps/native/src/app/login/index.tsx
+++ FE/apps/native/src/app/login/index.tsx
@@ -1,18 +1,32 @@
-import WebView from "react-native-webview";
+import { Text, TextInput, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { WEBVIEW_PATH } from "@/src/constants/webview";
-import { Redirect } from "expo-router";
 
 /**
  * TODO: 웹 - 앱 로그인 상태 동기화
  * TODO: 앱에 토큰 저장
  */
 
-export default function ProgramScreen() {
+export default function LoginScreen() {
   return (
-    // <SafeAreaView style={{ flex: 1 }}>
-    //   <WebView className="flex-1" source={{ uri: WEBVIEW_PATH.LOGIN }} />
-    // </SafeAreaView>
-    <Redirect href={"/(tabs)/home"} />
+    <SafeAreaView className="flex-1">
+      <Text></Text>
+      <View className="p-4">
+        <TextInput
+          placeholder="ID"
+          className="border rounded-lg p-4 border-gray-300 mb-4"
+        />
+        <TextInput
+          placeholder="PW"
+          className="border rounded-lg p-4 border-gray-300 mb-4"
+          secureTextEntry
+        />
+        <TouchableOpacity
+          onPress={() => {}}
+          className="px-4 py-2 rounded-full bg-primary"
+        >
+          <Text className="text-black text-center">Login</Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   );
 }
