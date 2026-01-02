e8dc6612469392910ab4f02a28fc570014d7c9bd
박건규 <geongyu09@gmail.com>
2026-01-02 22:46:16 +0900

    feat: axios 인스턴스 및 로그인 mutation 훅 구현

---
+++ FE/apps/native/src/apis/login.ts
@@ -0,0 +1,11 @@
+import { https } from "./instants";
+
+export const postLogin = async () => {
+  // const { data } = await https({
+  //   url: API.AUTH.SLACK_LOGIN,
+  //   method: "POST",
+  //   params: { code, redirect_uri },
+  // });
+  // return data;
+  return null;
+};
