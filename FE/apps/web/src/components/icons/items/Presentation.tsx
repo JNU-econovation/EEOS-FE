6fe638944ad675feee32182787adab54f1ff4daf
박건규 <geongyu09@gmail.com>
2026-02-14 23:48:39 +0900

    feat: 프레젠테이션 아이콘 SVG 및 컴포넌트 추가

---
+++ FE/apps/web/src/components/icons/items/Presentation.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+
+const Presentation = () => {
+  return (
+    <Image
+      src="/icons/presentation.svg"
+      width={24}
+      height={24}
+      alt="프레젠테이션"
+    />
+  );
+};
+
+export default Presentation;
