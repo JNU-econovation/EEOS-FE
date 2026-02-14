bb7976292d18e85a9daf510760703a7c6841f1e4
박건규 <geongyu09@gmail.com>
2026-02-14 18:41:26 +0900

    feat: 사용자 메타 섹션 컴포넌트 추가

---
+++ FE/apps/web/src/components/feature/webview-main/UserMetaSection.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { useGetMyActiveStatus } from "@/hooks/query/useUserQuery";
+
+const UserMetaSection = () => {
+  const { data, isLoading, isError } = useGetMyActiveStatus();
+
+  if (!data || isLoading || isError) return null;
+
+  const { name, activeStatus } = data;
+
+  return (
+    <section className="flex gap-2">
+      <div className="rounded-full bg-white px-2  py-1 text-sm  font-medium">
+        {name.split(" ")[0]}
+      </div>
+      <div className="rounded-full bg-white px-2  py-1 text-sm  font-medium">
+        디자이너
+      </div>
+      <div className="rounded-full bg-white px-2  py-1 text-sm  font-medium">
+        {activeStatus.toUpperCase()}
+      </div>
+    </section>
+  );
+};
+
+export default UserMetaSection;
