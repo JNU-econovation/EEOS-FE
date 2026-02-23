a82452c75c0f39b4e1bad5389afa5cb0c77a2889
박건규 <geongyu09@gmail.com>
2026-02-23 20:56:15 +0900

    feat: 앱 출석 현황 구현

--- FE/apps/web/src/app/(webview)/mobile/program/[programId]/page.tsx
+++ FE/apps/web/src/app/(webview)/mobile/program/[programId]/page.tsx
@@ -1,15 +1,19 @@
-import AppSafeArea from "@/components/common/AppSafeArea/AppSafeArea";
 import Spacing from "@/components/common/Spacing";
-import AttendeeInfoSection from "@/components/feature/detail/attendee/AttendeeInfoSection";
 import ProgramDashboardSection from "@/components/feature/detail/Dashboard/ProgramDashboardSection";
-import FireFIngerSection from "@/components/feature/detail/fireFinger/FireFIngerSection";
 import ProgramDetailSection from "@/components/feature/detail/program/ProgramDetailSection";
-import ProgramHeaderSection from "@/components/feature/detail/program/ProgramHeaderSection";
-import UserAttendModalSection from "@/components/feature/detail/userAttendModal/UserAttendModalSection";
 import WebviewFireFingerSection from "@/components/feature/webview-programs/WebviewFireFingerSection";
 import WebviewProgramAttendSection from "@/components/feature/webview-programs/WebviewProgramAttendSection";
 import WebviewProgramHeaderSection from "@/components/feature/webview-programs/WebviewProgramHeaderSection";
 import { SsgoiTransition } from "@ssgoi/react";
+import dynamic from "next/dynamic";
+
+const WebviewAttendeeInfoSection = dynamic(
+  () =>
+    import("@/components/feature/webview-programs/WebviewAttendeeInfoSection"),
+  {
+    ssr: false,
+  },
+);
 
 const ProgramDetailPage = () => {
   return (
@@ -24,16 +28,28 @@ const ProgramDetailPage = () => {
       <Spacing size={40} direction="vertical" unit="px" />
       <div className="h-3 bg-[#F5F5F5]" />
 
-      <section className="px-4">
-        {/* <ProgramHeaderSection /> */}
-        <div className="mt-12" />
+      <div className="mt-12" />
+      <div className="px-4">
         <WebviewFireFingerSection />
-        <div className="mt-12" />
-        <AttendeeInfoSection />
-        <div className="mt-12" />
+      </div>
+
+      <div className="mt-6" />
+      <div className="h-3 bg-[#F5F5F5]" />
+      <div className="mt-6" />
+
+      <div className="px-4">
+        <WebviewAttendeeInfoSection />
+      </div>
+
+      <div className="mt-6" />
+      <div className="h-3 bg-[#F5F5F5]" />
+      <div className="mt-6" />
+
+      <div className="px-4">
         <ProgramDashboardSection />
-        <div className="mt-24" />
-      </section>
+      </div>
+
+      <div className="mt-24" />
       {/* <UserAttendModalSection isLoggedIn /> */}
     </SsgoiTransition>
   );
---
+++ FE/apps/web/src/components/feature/webview-programs/WebviewAttendeeInfoSection.tsx
@@ -0,0 +1,71 @@
+"use client";
+
+import ErrorFallback from "@/components/common/error/ErrorFallback";
+import Tab from "@/components/common/tabs/tab/TabCompound/TabCompound";
+import Title from "@/components/common/Title/Title";
+import ATTEND_STATUS from "@/constants/ATTEND_STATUS";
+import { useGetProgramId } from "@/hooks/usePrograms";
+import { AttendStatus } from "@/types/member";
+import { ErrorBoundary } from "react-error-boundary";
+import AttendeeInfoByStatus from "../detail/attendee/AttendeeInfoByStatus";
+import AttendeeTabNumberBadge from "../detail/attendee/AttendeeTabNumberBadge";
+import Spacing from "@/components/common/Spacing";
+
+const WebviewAttendeeInfoSection = () => {
+  const programId = useGetProgramId();
+
+  return (
+    <section>
+      <Title text="출석 현황" textSize="xl" />
+      <div className="mt-8" />
+      <ErrorBoundary FallbackComponent={ErrorFallback}>
+        <AttendeeTab>
+          {({ status }) => (
+            <AttendeeInfoByStatus
+              key={status}
+              programId={programId}
+              status={status}
+            />
+          )}
+        </AttendeeTab>
+      </ErrorBoundary>
+    </section>
+  );
+};
+
+interface AttendeeTabProps {
+  children?: (selectedItem: { status: AttendStatus }) => React.ReactNode;
+}
+
+const AttendeeTab = ({ children }: AttendeeTabProps) => {
+  return (
+    <Tab<AttendStatus>
+      align="line"
+      defaultSelected={`${ATTEND_STATUS.STATUSES[0]}`}
+      nonPickedColor="white"
+      pickedColor="white"
+      tabSize="md"
+    >
+      <Tab.List className="!gap-0 border-b">
+        {ATTEND_STATUS.STATUSES.filter(
+          (status) => status !== "nonResponse",
+        ).map((status, index) => (
+          <Tab.NakedItem
+            key={`${status}-${index}`}
+            fullWidth
+            text={ATTEND_STATUS.LIST[status].text}
+            value={status}
+          >
+            <AttendeeTabNumberBadge status={status} />
+          </Tab.NakedItem>
+        ))}
+      </Tab.List>
+      <Spacing size={14} direction="vertical" unit="px" />
+      <Tab.Content<AttendStatus>>
+        {({ selectedItem }) => children({ status: selectedItem })}
+      </Tab.Content>
+    </Tab>
+  );
+};
+
+export default WebviewAttendeeInfoSection;
