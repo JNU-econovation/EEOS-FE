1b068d499c2473fb557edfc4012182e610ba07d0
박건규 <geongyu09@gmail.com>
2026-02-23 12:12:13 +0900

    feat: 일정 생성 모달을 닫는 X 버튼 구현

--- FE/apps/native/src/app/createEvent/[date].tsx
+++ FE/apps/native/src/app/createEvent/[date].tsx
@@ -1,7 +1,8 @@
-import { Text, View } from "react-native";
+import X from "@/assets/icons/X.svg";
+import { router, useLocalSearchParams } from "expo-router";
+import { Text, TouchableOpacity, View } from "react-native";
 import WebviewWithInjected from "../../components/WebviewWithInjected";
 import { WEBVIEW_PATH } from "../../constants/webview";
-import { router, useLocalSearchParams } from "expo-router";
 
 const createEvent = () => {
   const { date: selectedTimestamp } = useLocalSearchParams<{ date: string }>();
@@ -9,14 +10,12 @@ const createEvent = () => {
   return (
     <View className="flex-1 bg-white">
       <View className="py-8 flex-row items-center justify-center">
-        <Text
-          className="text-xl absolute left-8"
-          onPress={() => {
-            router.back();
-          }}
+        <TouchableOpacity
+          onPress={() => router.back()}
+          className="absolute left-8"
         >
-          X
-        </Text>
+          <X />
+        </TouchableOpacity>
         <Text className="text-xl">일정 등록</Text>
       </View>
       <WebviewWithInjected
