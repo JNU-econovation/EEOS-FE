22a2f26cf9780dad517295f8f5a4984d1b0825f9
박건규 <geongyu09@gmail.com>
2026-03-22 22:54:26 +0900

    feat: 메인화면 일정 베너 변경된 정책에 맞추어 변경

--- FE/apps/web/src/app/(webview)/mobile/main/page.tsx
+++ FE/apps/web/src/app/(webview)/mobile/main/page.tsx
@@ -3,26 +3,31 @@ import BellIcon from "@/components/common/icons/BellIcon";
 import LogoIcon from "@/components/common/icons/LogoIcon";
 import Spacing from "@/components/common/Spacing";
 import HelloSection from "@/components/feature/webview-main/HelloSection";
-import TodayEventSection from "@/components/feature/webview-main/TodayEventSection";
+import HomeEventSection from "@/components/feature/webview-main/HomeEventSection";
 // import UserMetaSection from "@/components/feature/webview-main/UserMetaSection";
 import { SsgoiTransition } from "@ssgoi/react";
 
 const WebviewMainPage = () => {
   return (
     <SsgoiTransition id="/main" className="min-h-screen bg-white">
-      <div>
-        <section className="bg-[#F2F2F7] px-6 pt-16">
-          <div className="flex items-center justify-between">
-            <LogoIcon />
-            <BellIcon />
-          </div>
-          <div className="h-6" />
-          <HelloSection />
+      <div className="flex h-screen flex-col">
+        <div>
+          <section className="relative z-10 bg-[#F2F2F7] px-6 pt-16 shadow-[0px_-10px_10px_0px_rgba(34,43,69,0.21)]">
+            <div className="flex items-center justify-between">
+              <LogoIcon />
+              <BellIcon />
+            </div>
+            <div className="h-6" />
+            <HelloSection />
 
-          <Spacing size={74} direction="vertical" unit="px" />
-          <TodayEventSection />
-          {/* <Spacing size={24} direction="vertical" unit="px" /> */}
-        </section>
+            <Spacing size={74} direction="vertical" unit="px" />
+            <div className="z-50">
+              <HomeEventSection />
+            </div>
+            {/* <Spacing size={24} direction="vertical" unit="px" /> */}
+          </section>
+        </div>
+        <div className="grow shadow-[0px_-100px_10px_0px_rgba(34,43,69,0.21)]"></div>
       </div>
     </SsgoiTransition>
   );
