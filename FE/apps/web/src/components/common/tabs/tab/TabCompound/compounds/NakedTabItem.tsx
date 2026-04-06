cedfc16b2142eb30ea7de6e24e88e3707d5ffc6e
박건규 <geongyu09@gmail.com>
2026-04-06 17:12:12 +0900

    feat: 질문 게시판 팀 탭에 shrink-0 속성 추가

--- FE/apps/web/src/components/common/tabs/tab/TabCompound/compounds/NakedTabItem.tsx
+++ FE/apps/web/src/components/common/tabs/tab/TabCompound/compounds/NakedTabItem.tsx
@@ -21,7 +21,7 @@ function NakedTabItem<T extends string>({
   const border = isSelected ? "border-black" : "border-transparent";
 
   const tabItemStyle = classNames(
-    "relative flex h-fit w-fit cursor-pointer items-center justify-center gap-2 border-b-2 border-black font-semibold text-black transition-all",
+    "relative flex h-fit w-fit shrink-0 cursor-pointer items-center justify-center gap-2 border-b-2 border-black font-semibold text-black transition-all",
     tabSizes[tabSize],
     border,
     fullWidth ? "w-full" : "",
